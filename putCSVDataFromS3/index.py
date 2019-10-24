865dca5a115ff274c6b5ec304b46609dd551fb7d
Robert <bkuhlke@yahoo.com>
2019-10-24 15:12:03 -0400

    adding lambda functions to interact with s3

--- putCSVDataFromS3/index.py
+++ putCSVDataFromS3/index.py
@@ -1,8 +1,14 @@
 import boto3
 
-S3 = boto3.client('s3')
-dynamoDB = boto3.resources('dynamodb')
+s3_client = boto3.client('s3')
 
-def csv_reader(event, context):
 
-    print(event)
+def lambda_handler(event, context):
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    s3_filename = event['Records'][0]['s3']['object']['key']
+    resp = s3_client.get_object(Bucket=bucket_name, Key=s3_filename)
+    data = resp['Body'].read().decode('utf-8')
+    employees = data.split('\\n')
+    for emp in employees:
+        print(employees)
+        # adding to dynamodb
